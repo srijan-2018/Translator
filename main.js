c56fd758320365bccf603f4e6afde52942e45b55
srijan-2018 <srijandas2018@gmail.com>
2022-04-22 08:03:06 +0530

    file changes js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fromText = document.querySelector(".from-text"),
     icons = document.querySelectorAll(".row i");
 translateBtn = document.querySelector("button"), selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "bn-IN" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
